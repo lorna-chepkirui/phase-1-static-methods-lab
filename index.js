e52c7d401f425f8009fbb8c7fe8be0e7cd20fa0c
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-06-28 14:04:03 +0300

    commit tree 529e73840c380ecf3c8cf1139fd920aaa689b1ea

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
   //add static methods here
+  static capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^'-\w\s]/gi, "");
+  }
+  static titleize(string) {
+    let nonTitlelized = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+
+    let words = string.split(" ");
+    let firstWord = string.slice(0, 1)[0].toUpperCase() + words[0].slice(1);
+
+    words = [firstWord, ...words.slice(1)];
+    return words
+      .map((word) =>
+        nonTitlelized.includes(word)
+          ? word
+          : word[0].toUpperCase() + word.slice(1)
+      )
+      .join(" ");
+  }
 }
